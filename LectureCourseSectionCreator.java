6cb11ff22aaebf88e5fbebc61e956903c6f912a1
Ahmad <Ahmadsaadi.x77@gmail.com>
2024-05-15 06:35:14 +0300

    Refactor `LectureCourseSectionCreator` to update method parameters

--- LectureCourseSectionCreator.java
+++ LectureCourseSectionCreator.java
@@ -1,7 +1,6 @@
 public class LectureCourseSectionCreator extends CourseSectionCreator {
     @Override
-    public CourseSection createCourseSection(String subject, String number, String name, String id, String days,
-            String startTime, String building, String room) {
-        return new CourseSection(subject, number, name, id, days, startTime, building, room);
+    public CourseSection createCourseSection(String courseId, String courseName, String sectionId, String days, String startTime, String building, String room) {
+        return new CourseSection( courseId, courseName, sectionId, days, startTime, building, room);
     }
 }
